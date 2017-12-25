dd44b6e3248f22c671a396d79184af9ee9546584
FishDen <fishden1333@gmail.com>
2017-12-25 18:20:14 +0800

    Update thresholding

--- thresholding.m
+++ thresholding.m
@@ -1,14 +1,15 @@
 % input: original image (type single)
 % output: thresholded image (type single)
-function output = thresholding(input, threshold)
+% belowThreValue: the value assigned for pixels below threshold
+function output = thresholding(input, threshold, belowThreValue)
     [M, N] = size(input);
     output = zeros(M, N);
     for x = 1 : M
         for y = 1 : N
             if input(x, y) < threshold 
-                output(x, y) = 0;
+                output(x, y) = belowThreValue;
             else
-                output(x, y) = 1;
+                output(x, y) = 1 - belowThreValue;
             end
         end
     end
