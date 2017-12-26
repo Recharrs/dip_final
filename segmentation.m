41c2234543b939ba0652a3e2a72b97b1f8d1d096
FishDen <fishden1333@gmail.com>
2017-12-26 20:22:20 +0800

    Update segmentation

--- segmentation.m
+++ segmentation.m
@@ -1,7 +1,7 @@
 % I: original image (3D, type single)
 % blurSize: size of averaging filter (odd number prefered)
 % Is: segmented image, only foreground is colored (3D)
-function Is = segmentation(I)
+function [Is1, Is2] = segmentation(I)
     % Make sure the image is in type single
     I = im2single(I);
     Ib = I;
@@ -17,17 +17,14 @@ function Is = segmentation(I)
     % Apply K-means segmentation algorithm
     Is = kMeansSegment(Ib);
     
-    % Thresholding the blurred image, only the foreground has value 1
+    % Thresholding the blurred image, make 2 pictures
+    % One with only foreground, one eith only background
     % thre: threshold
     thre = 1;
-    It = thresholding(Is, thre, 1);
+    It1 = thresholding(Is, thre, 1);
+    It2 = thresholding(Is, thre, 0);
     
-    % If foreground and background is switched, change back
-    [M, N] = size(It);
-    if It(floor(M / 2), floor(N / 2)) == 0
-        It = 1 - It;
-    end
-    
-    % Make the foreground colored only
-    Is = noBackground(I, It);
+    % Color the selected region only for the 2 pictures
+    Is1 = noBackground(I, It1);
+    Is2 = noBackground(I, It2);
 end
