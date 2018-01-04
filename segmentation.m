7f51e92c72bc799c834ca1885082c8b3a3d0aec4
FishDen <fishden1333@gmail.com>
2018-01-04 14:24:25 +0800

    Update segmentation with hole filling

--- segmentation.m
+++ segmentation.m
@@ -1,7 +1,7 @@
 % I: original image (3D, type single)
 % blurSize: size of averaging filter (odd number prefered)
 % Is: segmented image, only foreground is colored (3D)
-function [Is1, Is2, It1, It2] = segmentation(I)
+function [If1, If2, It1, It2] = segmentation(I)
     % Make sure the image is in type single
     I = im2single(I);
     Ib = I;
@@ -24,7 +24,11 @@ function [Is1, Is2, It1, It2] = segmentation(I)
     It1 = thresholding(Is, thre, 1);
     It2 = thresholding(Is, thre, 0);
     
+    % Fill holes in the thresholded images
+    It1 = imfill(It1, 'holes');
+    It2 = imfill(It2, 'holes');
+    
     % Color the selected region only for the 2 pictures
-    Is1 = noBackground(I, It1);
-    Is2 = noBackground(I, It2);
+    If1 = noBackground(I, It1);
+    If2 = noBackground(I, It2);
 end
